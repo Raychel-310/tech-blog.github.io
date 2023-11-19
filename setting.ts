a70654601e9618ab15ea2090ae8c4ffa4231c538
Ray310 <128213529+Raychel-310@users.noreply.github.com>
2023-11-19 10:31:09 +0000

    setup ogp

--- setting.ts
+++ setting.ts
@@ -1,2 +1,2 @@
 // ex) export const TARGET_URL = "https://xxxx.github.io/tech-blog.github.io/";
-export const TARGET_URL = "https://";
+export const TARGET_URL = "https://raychel-310.github.io/tech-blog.github.io/";
